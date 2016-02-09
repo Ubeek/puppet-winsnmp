520286b7a35d1d0c68d0cc10f0b55385e222408b
Levi Jarick <git@ubeek.net>
2016-02-09 10:54:37 +0800

    Removed duplicate reg key declaration

--- manifests/permitted_managers.pp
+++ manifests/permitted_managers.pp
@@ -3,9 +3,6 @@ define winsnmp::permitted_managers (
 ) {
   $path = 'HKLM\SYSTEM\CurrentControlSet\services\SNMP\Parameters\PermittedManagers'
 
-  #Create above reg key only if it doesn't exist; Allows for multiple managers to be defined without running into multiple declarations of the same resource
-  ensure_resource('registry_key', $path, {'ensure' => 'present' })
-
   if ! empty($managers) {
     $keys = keys($managers)
     winsnmp::managers { $keys:
